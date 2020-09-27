1ad506e5a5dd2c1171206e7d908874771ad31e0f
Martin Pordomingo <martinpordomingo@gmail.com>
2020-09-27 16:13:54 -0300

    extra code removed

--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -7,46 +7,6 @@ import '../resources/styles/topBar.scss'
 import {slide as Menu} from 'react-burger-menu'
 export default class TopBar extends React.Component<any, any>  {
 
-  constructor(props){
-    super(props);
-    this.state = { consumptions: [], token: null, displayDrawer: false}
-  }
-
-  componentDidMount(){
-    this.setState({token: this.getAuthToken()})
-    this.fetchConsumptions();
-  }
-
-  fetchConsumptions(){
-    var myHeaders = new Headers();
-    myHeaders.append("Authorization", "Bearer " + this.getAuthToken());
-
-    let requestOptions: RequestInit = {
-      method: 'GET',
-      headers: myHeaders,
-      redirect: 'follow'
-    };
-
-    fetch("https://localhost:5001/consumptions", requestOptions)
-      .then(response => response.json())
-      .then(result => { this.setState({...this.state, consumptions: result }); })
-      .catch(error => console.log('error', error));
-  }
-
-  getAuthToken(){
-    let token = null;
-    if (document.cookie.split(';').some((item) => item.trim().startsWith('token='))) {
-      token = document.cookie
-        .split("; ")
-        .find(row => row.startsWith("token"))
-        .split("=")[1];
-      }
-    return token;
-  }
-
-  handleDrawerOpen = () => {
-    this.setState({displayDrawer: !this.state.displayDrawer});
-  };
 
   render(){
     var i = 0;
