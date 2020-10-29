ca7d95e520482eb42ab82ebebfe112e2f4f339a1
Martín García Tejeda <m.garcia.tejeda@gmail.com>
2020-10-29 02:58:30 -0300

    Mejoras de categorias

--- components/CategoryForm.tsx
+++ components/CategoryForm.tsx
@@ -1,55 +1,65 @@
-import * as React from 'react'
-import '../resources/styles/addConsumptionForm.scss'
-import {TextField, Select, InputLabel, FormControl, Button} from '@material-ui/core';
-import AuthService  from '../utils/AuthService.tsx';
-import IconButton from '@material-ui/core/IconButton';
-import AddButton from '@material-ui/icons/Add';
-import ClearButton from '@material-ui/icons/Clear';
-import Categories from '../components/Categories';
+import * as React from "react";
+import "../resources/styles/addConsumptionForm.scss";
 import {
-    postCategory,
-    fetchCategories,
-    updateCategory,
-    deleteCategory
-} from '../controllers/CategoriesController.tsx';
+  TextField,
+  Select,
+  InputLabel,
+  FormControl,
+  Button,
+} from "@material-ui/core";
+import AuthService from "../utils/AuthService.tsx";
+import IconButton from "@material-ui/core/IconButton";
+import AddButton from "@material-ui/icons/Add";
+import ClearButton from "@material-ui/icons/Clear";
+import Categories from "../components/Categories";
+import CategoryIconsList from "../components/CategoryIconsList";
+import {
+  postCategory,
+  fetchCategories,
+  updateCategory,
+  deleteCategory,
+} from "../controllers/CategoriesController.tsx";
 
-
-
-export default class CategoryForm extends React.Component<any,any>{
-  constructor(props){
+export default class CategoryForm extends React.Component<any, any> {
+  constructor(props) {
     super(props);
     this.state = {
       categoryName: "",
-      creationStatus:"empty",
+      categoryIcon: "",
+      creationStatus: "empty",
       errorMessages: [],
-      categoryNameControl: {error: false, helperText: ""},
-      amountControl: {error: false, helperText: ""}
-    }
+      categoryNameControl: { error: false, helperText: "" },
+      amountControl: { error: false, helperText: "" },
+    };
   }
 
-  componentDidMount(){
-    if (this.props.task.operation === "edit"){
+  componentDidMount() {
+    if (this.props.task.operation === "edit") {
       fetchCategories(this.props.task.categoryId)
-      .then(result => {
-        this.setState({
-          categoryName: result.name
-        }); })
-      .catch(error => console.log('error', error));
+        .then((result) => {
+          this.setState({
+            categoryName: result.name,
+          });
+        })
+        .catch((error) => console.log("error", error));
     }
   }
 
-  handleChange = (e) =>{
-      this.setState({[e.target.name]: e.target.value});
-  }
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
 
   formIsValid() {
     var error = true;
 
     let categoryName = this.state.categoryName;
 
-    if (categoryName == null || categoryName == ""){
+    if (categoryName == null || categoryName == "") {
       this.setState({
-        categoryNameControl: {error: true, helperText: "Se debe especificar un nombre de categoria."}
+        categoryNameControl: {
+          error: true,
+          helperText: "Se debe especificar un nombre de categoria.",
+        },
       });
       error = false;
     }
@@ -58,75 +68,88 @@ export default class CategoryForm extends React.Component<any,any>{
   }
 
   onSubmit = (e) => {
-      e.preventDefault();
-      var error = false;
-      let errorMessages = this.state.errorMessages;
+    e.preventDefault();
+    var error = false;
+    let errorMessages = this.state.errorMessages;
 
+    let payload = {
+      name: this.state.categoryName,
+      icon: this.state.categoryIcon,
+    };
 
-      let payload = {
-        name: this.state.categoryName,
-        icon: 'exampleIcon'
-     };
+    if (!this.formIsValid()) {
+      return;
+    }
 
-      if(!this.formIsValid()){return;}
-
-      if(this.props.task.operation === "edit"){
-        updateCategory(this.props.categoryId, payload).then(res => {
-          console.log(res);
-          if(res.result == "error"){
-            this.setState({status: "error", errorMessages: res.messages});
-          }
-          else {
-              this.props.onEditionOk();
-          }
-        });
-      }
-      else if (this.props.task.operation === "add"){
-        postCategory(payload).then(res => {
-          console.log(res);
-          if(res.result == "error"){
-            this.setState({status: "error", errorMessages: res.messages});
-          }
-          else {
-              this.props.onCreationOk();
-          }
-        });
-      }
-  }
+    if (this.props.task.operation === "edit") {
+      updateCategory(this.props.categoryId, payload).then((res) => {
+        console.log(res);
+        if (res.result == "error") {
+          this.setState({ status: "error", errorMessages: res.messages });
+        } else {
+          this.props.onEditionOk();
+        }
+      });
+    } else if (this.props.task.operation === "add") {
+      postCategory(payload).then((res) => {
+        console.log(res);
+        if (res.result == "error") {
+          this.setState({ status: "error", errorMessages: res.messages });
+        } else {
+          this.props.onCreationOk();
+        }
+      });
+    }
+  };
 
   onCancel = (e) => {
-    this.setState({ categoryName: ""});
-    if(this.props.onCancel !== null)
-      this.props.onCancel();
-  }
+    this.setState({ categoryName: "" });
+    if (this.props.onCancel !== null) this.props.onCancel();
+  };
 
-  
-  render(){
-    return <div className="addContsumptionFormContainer">
-      <h2 id="transition-modal-title" className="addConsumptionFormTitle">
-        Agregar categoria
-      </h2>
-      <form className="addContsumptionForm">
-        <div className="addConsumptionFormItem">
-          <TextField name="categoryName" autoComplete="off"
-          className="addConsumptionField"
-          InputProps= {{ style: { fontSize:"0.9rem"}}}
-          label="Nombre"
-          value={this.state.categoryName}
-          error={this.state.categoryNameControl.error}
-          helperText={this.state.categoryNameControl.helperText}
-          onChange={this.handleChange}/>
-        </div>
-        
-        
-        <div className="addConsumptionFormItem">
-          <Button  color="primary" onClick={this.onSubmit}>{
-            (this.props.task.operation === "edit")?
-            "Guardar":"Crear"
-          }</Button>
-          <Button  color="primary" onClick={this.onCancel}>Cancelar</Button>
-        </div>
-      </form>
-    </div>;
+  render() {
+    return (
+      <div className="addContsumptionFormContainer">
+        <h2 id="transition-modal-title" className="addConsumptionFormTitle">
+          Agregar categoria
+        </h2>
+        <form className="addContsumptionForm">
+          <div className="addCategoryFormItem">
+            <TextField
+              name="categoryName"
+              autoComplete="off"
+              className="addConsumptionField"
+              InputProps={{ style: { fontSize: "0.9rem" } }}
+              label="Nombre"
+              value={this.state.categoryName}
+              error={this.state.categoryNameControl.error}
+              helperText={this.state.categoryNameControl.helperText}
+              onChange={this.handleChange}
+            />
+            <FormControl className="addConsumptionFormSelect">
+              <InputLabel
+                className="category-icon-label"
+                htmlFor="category-icon-select"
+              >
+                Icono de categoria:{" "}
+              </InputLabel>
+              <CategoryIconsList
+                value={this.state.categoryIcon}
+                onChange={this.handleChange}
+              />
+            </FormControl>
+          </div>
+
+          <div className="addConsumptionFormItem">
+            <Button color="primary" onClick={this.onSubmit}>
+              {this.props.task.operation === "edit" ? "Guardar" : "Crear"}
+            </Button>
+            <Button color="primary" onClick={this.onCancel}>
+              Cancelar
+            </Button>
+          </div>
+        </form>
+      </div>
+    );
   }
 }
--- controllers/CategoriesController.tsx
+++ controllers/CategoriesController.tsx
@@ -1,84 +1,122 @@
-export  async function postCategory(data) {
+import { defaultCategories } from "../utils/DefaultCategories";
+import CategoryIcons from "../utils/CategoryIcons.js";
+
+export async function postCategory(data) {
   let token = getAuthToken();
 
   const response = await fetch("https://localhost:5001/categories", {
-    method: 'POST',
-    mode: 'cors',
-    cache: 'no-cache',
-    credentials: 'same-origin',
+    method: "POST",
+    mode: "cors",
+    cache: "no-cache",
+    credentials: "same-origin",
     headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer ' + token
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + token,
     },
-    redirect: 'follow',
-    referrerPolicy: 'no-referrer',
-    body: JSON.stringify(data)
+    redirect: "follow",
+    referrerPolicy: "no-referrer",
+    body: JSON.stringify(data),
   });
   return response.json();
 }
 
-export async function fetchCategories(category){
-
-    var myHeaders = new Headers();
-    myHeaders.append("Authorization", "Bearer " + getAuthToken());
-    let requestOptions: RequestInit = {
-      method: 'GET',
-      headers: myHeaders,
-      redirect: 'follow'
-    };
-
-    return fetch("https://localhost:5001/categories", requestOptions)
-      .then(response => response.json());
+export async function postDefaultCategories() {
+  let token = getAuthToken();
+  for (let index = 0; index < defaultCategories.length; ++index) {
+    await fetch("https://localhost:5001/categories", {
+      method: "POST",
+      mode: "cors",
+      cache: "no-cache",
+      credentials: "same-origin",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token,
+      },
+      redirect: "follow",
+      referrerPolicy: "no-referrer",
+      body: JSON.stringify(defaultCategories[index]),
+    });
+  }
 }
 
-export  async function updateCategory(id, data) {
+export async function fetchCategories(category) {
+  var myHeaders = new Headers();
+  myHeaders.append("Authorization", "Bearer " + getAuthToken());
+  let requestOptions: RequestInit = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+  };
+
+  return fetch(
+    "https://localhost:5001/categories",
+    requestOptions
+  ).then((response) => response.json());
+}
+
+export async function updateCategory(id, data) {
   let token = getAuthToken();
-  var fetchURL = "https://localhost:5001/categories/"+id;
+  var fetchURL = "https://localhost:5001/categories/" + id;
   const response = await fetch(fetchURL, {
-    method: 'PUT',
-    mode: 'cors',
-    cache: 'no-cache',
-    credentials: 'same-origin',
+    method: "PUT",
+    mode: "cors",
+    cache: "no-cache",
+    credentials: "same-origin",
     headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer ' + token
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + token,
     },
-    redirect: 'follow',
-    referrerPolicy: 'no-referrer',
-    body: JSON.stringify(data)
-  })
-  .catch(error => console.log('error', error));
+    redirect: "follow",
+    referrerPolicy: "no-referrer",
+    body: JSON.stringify(data),
+  }).catch((error) => console.log("error", error));
 
   return response.json();
 }
 
-export  async function deleteCategory(id) {
+export async function deleteCategory(id) {
   let token = getAuthToken();
-  var fetchURL = "https://localhost:5001/categories/"+id;
+  var fetchURL = "https://localhost:5001/categories/" + id;
   const response = await fetch(fetchURL, {
-    method: 'DELETE',
-    mode: 'cors',
-    cache: 'no-cache',
-    credentials: 'same-origin',
+    method: "DELETE",
+    mode: "cors",
+    cache: "no-cache",
+    credentials: "same-origin",
     headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer ' + token
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + token,
     },
-    redirect: 'follow',
-    referrerPolicy: 'no-referrer'
-  })
-  .catch(error => console.log('error', error));
+    redirect: "follow",
+    referrerPolicy: "no-referrer",
+  }).catch((error) => console.log("error", error));
 
   return response;
 }
 
-function getAuthToken(){
+function getAuthToken() {
   let token = null;
-  if (document.cookie.split(';').some((item) => item.trim().startsWith('token='))) {
+  if (
+    document.cookie.split(";").some((item) => item.trim().startsWith("token="))
+  ) {
     token = document.cookie
       .split("; ")
-      .find(row => row.startsWith("token"))
+      .find((row) => row.startsWith("token"))
       .split("=")[1];
-    }
+  }
   return token;
+}
+
+export function getCategoryIcon(category) {
+  for (const c in CategoryIcons) {
+    if (CategoryIcons[c].name == category) {
+      var path = "/images/categories/" + CategoryIcons[c].path;
+      return (
+        <img
+          src={path}
+          alt={CategoryIcons[c].name}
+          className="categoryIconSmall"
+        />
+      );
+    }
+  }
 }
